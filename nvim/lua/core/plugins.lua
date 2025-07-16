c3b44019c818bc1ff41cfb7104eefab89881f403
gbaechler12 <guilhem.baechler@gmail.com>
2025-07-16 19:03:23 +0200

    add render_markdown

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -130,6 +130,17 @@ return require('packer').startup(function(use)
   }
 
   use "neovim/nvim-lspconfig"
+
+  use {
+    'MeanderingProgrammer/render-markdown.nvim',
+    after = { 'nvim-treesitter' },
+    -- requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+    -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+    -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+    config = function()
+        require('render-markdown').setup({})
+    end,
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
